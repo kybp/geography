1d94a0eb8f34c6e7fa78fdd463e4dfc2c3cfe0c2
Kyle Brown <picokyle@gmail.com>
2016-08-17 04:43:09 -0600

    Add all provinces and territories

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
+// -*- typescript -*-
 import * as React from 'react'
 import { render } from 'react-dom'
 import NorthAmerica from './maps/na'
 
-const SVG_HEIGHT = 600
-const SVG_WIDTH  = 800
+const SVG_HEIGHT = 1245
+const SVG_WIDTH  = 1285
 
 render(
   <svg height={ SVG_HEIGHT } width={ SVG_WIDTH } style={{
--- src/maps/na/ca/index.tsx
+++ src/maps/na/ca/index.tsx
@@ -1,10 +1,22 @@
+// -*- typescript -*-
 import * as React from 'react'
 import * as provinces from './provinces'
 
 const Canada = () => (
   <g>
-    <provinces.BC fill="lightgreen" />
-    <provinces.Alberta fill="mediumpurple" />
+    <provinces.BC fill="goldenrod"    />
+    <provinces.AB fill="mediumpurple" />
+    <provinces.SK fill="lightgreen"   />
+    <provinces.MB fill="lightpink"    />
+    <provinces.ON fill="orange"       />
+    <provinces.QC fill="darkgreen"    />
+    <provinces.NU fill="salmon"       />
+    <provinces.NB fill="blue"         />
+    <provinces.NS fill="darkmagenta"  />
+    <provinces.NL fill="hotpink"      />
+    <provinces.YT fill="indianred"    />
+    <provinces.NT fill="lightyellow"  />
+    <provinces.PE fill="grey"         />
   </g>
 )
 
