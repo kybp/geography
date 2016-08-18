14c11de8d047778809c26b0677bfcb302d82a431
Kyle Brown <picokyle@gmail.com>
2016-08-18 03:43:09 -0600

    Refactor
    I removed the helper functions for province definitions and cleaned
    up that whole file. I also removed our dependency on Immutable.

--- src/maps/Point.ts
+++ src/maps/Point.ts
@@ -1,13 +1,9 @@
-import { List } from 'immutable'
-
 export class Point {
   constructor(public x: number, public y: number) {}
 }
 
-export type PointSet = List<Point>
-
 export const makePointString =
-  (points: PointSet, xOffset: number, yOffset: number) => (
+  (xOffset: number, yOffset: number, points: Point[]) => (
     points
       .map(({ x, y }) => (x + xOffset) + ' ' + (y + yOffset))
       .join(', ')
