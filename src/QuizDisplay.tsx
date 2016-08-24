df02421fa81e44298e8c2b0bde5a5fe2b8d1d8d6
Kyle Brown <picokyle@gmail.com>
2016-08-24 16:34:35 -0600

    Give a score after the quiz

--- src/QuizDisplay.tsx
+++ src/QuizDisplay.tsx
@@ -1,13 +1,21 @@
-// -*- web -*-
+// -*- typescript -*-
 import * as React from 'react'
 import { connect } from 'react-redux'
 import Canada from './maps/na/ca'
+
+const getScore = () => {
+  // document.querySelectorAll('.right') gives 1 less than it should
+  const wrong = document.querySelectorAll('.wrong').length
+  const total = document.querySelectorAll('.province').length
+  const right = total - wrong
+  return right + '/' + total
+}
 
 const QuizDisplay = ({ targetProvince }) => (
   <div>
     <h1>{ targetProvince
           ? 'Click on ' + targetProvince.displayName
-          : 'The end' }</h1>
+          : 'You got ' + getScore() + ' correct' }</h1>
     <svg viewBox="0 0 1285 1245" height={ 300 } width={ 300 } 
          style={{ background: 'lightblue' }}>
       <Canada xOffset={ 0 } yOffset={ 0 } provinceStates={ [] } />
